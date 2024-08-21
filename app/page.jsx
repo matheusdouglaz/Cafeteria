b4d709fccf19d51d411cc1db1925ae788d1e8bc2
matheusdouglaz <matheustecnologi@outlook.com>
2024-08-21 17:17:47 -0300

    Footer finalizado

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,9 @@ import Hero from "@/components/Hero";
 import Explore from "@/components/Explore";
 import About from "@/components/About";
 import Menu from "@/components/Menu";
+import Testimonials from "@/components/Testimonials";
+import OpeningHours from "@/components/OpeningHours";
+
 
 
 const Home = () => {
@@ -23,8 +26,10 @@ const Home = () => {
       <Explore/>
       <About />
       <Menu />
+      <OpeningHours />
+      <Testimonials />
       {/*temporary*/}
-      <div className="h-[4000px]"></div>
+      {/*<div className="h-[4000px]"></div>*/}
     </div>
   );
 };
