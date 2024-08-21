3a5e964ff6adc8ae8d514719da1e3e4ec09e180a
matheusdouglaz <matheustecnologi@outlook.com>
2024-08-21 11:20:55 -0300

    Menu finalizado

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import Hero from "@/components/Hero";
 import Explore from "@/components/Explore";
 import About from "@/components/About";
+import Menu from "@/components/Menu";
 
 
 const Home = () => {
@@ -21,6 +22,7 @@ const Home = () => {
       <Hero />
       <Explore/>
       <About />
+      <Menu />
       {/*temporary*/}
       <div className="h-[4000px]"></div>
     </div>
